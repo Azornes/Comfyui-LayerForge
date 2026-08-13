7709eeb49f1d237226cd5b58e3613e7465eb9103
Dariusz L <dl122@wp.pl>
2026-08-13 02:04:15 +0200

    refactor: remove legacy logging compatibility

--- python/__init__.py
+++ python/__init__.py
@@ -1,5 +1,1 @@
-# This file makes the 'python' directory a package.
-from . import log_system
-from .log_system import logger
-
-__all__ = ['log_system', 'logger']
+"""Backend package for the LayerForge ComfyUI custom node."""
