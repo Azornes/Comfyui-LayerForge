fc4292ecb0ff70956d15e9633e9b8a093e2b7183
Dariusz L <dl122@wp.pl>
2026-08-14 15:56:31 +0200

    ci: enforce generated frontend and coverage

--- build.bat
+++ build.bat
@@ -4,9 +4,12 @@ robocopy src/css js/css /E /nfl /ndl /njh /njs > nul
 robocopy src/templates js/templates /E /nfl /ndl /njh /njs > nul
 echo Compiling TypeScript files...
 call .\\node_modules\\.bin\\tsc.cmd
-if %errorlevel% equ 0 (
+set BUILD_ERROR=%errorlevel%
+if %BUILD_ERROR% equ 0 (
     echo Build completed successfully - no errors found.
 ) else (
-    echo Build failed with error code %errorlevel%.
+    echo Build failed with error code %BUILD_ERROR%.
+    exit /b %BUILD_ERROR%
 )
 echo Build process finished.
+exit /b 0
